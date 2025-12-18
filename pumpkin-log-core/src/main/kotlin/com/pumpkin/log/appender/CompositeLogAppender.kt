d29970af4d1c0b8a2842617fb43ea63c1baed03f
LeeHyungGeol <rjf1138@naver.com>
2025-12-19 02:40:07 +0900

    feat: CompositeLogAppender에 onError 콜백 추가

--- pumpkin-log-core/src/main/kotlin/com/pumpkin/log/appender/CompositeLogAppender.kt
+++ pumpkin-log-core/src/main/kotlin/com/pumpkin/log/appender/CompositeLogAppender.kt
@@ -3,7 +3,8 @@ package com.pumpkin.log.appender
 import com.pumpkin.log.model.HttpLog
 
 class CompositeLogAppender(
-    private val appenders: List<LogAppender>
+    private val appenders: List<LogAppender>,
+    private val onError: ((LogAppender, Throwable) -> Unit)? = null
 ) : LogAppender {
 
     constructor(vararg appenders: LogAppender) : this(appenders.toList())
@@ -11,6 +12,7 @@ class CompositeLogAppender(
     override fun append(log: HttpLog) {
         appenders.forEach { appender ->
             runCatching { appender.append(log) }
+                .onFailure { e -> onError?.invoke(appender, e)}
         }
     }
 }
